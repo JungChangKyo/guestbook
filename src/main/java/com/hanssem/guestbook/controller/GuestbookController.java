fa49812c0b98919caa714d2ac979edbc762bd62d
JungChangKyo <jnngchangkyo@gmail.com>
2022-08-26 14:16:32 +0900

    commit test

--- src/main/java/com/hanssem/guestbook/controller/GuestbookController.java
+++ src/main/java/com/hanssem/guestbook/controller/GuestbookController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class GuestbookController {
     @GetMapping({"/","/list"})
     public String list() {
-        log.info("list....");
+        log.info("list.....");
         return "/guestbook/list";
     }
 }
