16d8a2b68ae7c3521c60ca86f639aeabec7f3ce4
JungChangKyo <jnngchangkyo@gmail.com>
2022-08-26 14:10:51 +0900

    Initial commit

---
+++ src/main/java/com/hanssem/guestbook/repository/GuestbookRepository.java
@@ -0,0 +1,9 @@
+package com.hanssem.guestbook.repository;
+
+import com.hanssem.guestbook.entity.Guestbook;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface GuestbookRepository extends JpaRepository<Guestbook, Long>,
+        QuerydslPredicateExecutor<Guestbook> {
+}
